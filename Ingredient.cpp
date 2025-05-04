c2542a9bcf9429f4389fa33b14f08486107fb477
laptiuchka <tanialapik@gmail.com>
2025-05-04 19:09:21 +0300

    I added copy and move constructors and operators

--- Ingredient.cpp
+++ Ingredient.cpp
@@ -2,43 +2,8 @@
 #include <iostream>
 
 Ingredient::Ingredient(const std::string& name, double quantity, const std::string& unit)
-    : Item(name), quantity(quantity), unit(unit) {}
-
-Ingredient::Ingredient(const Ingredient& other)
-    : Item(other.name), quantity(other.quantity), unit(other.unit) {
-    std::cout << "Copying Ingredient: " << other.name << std::endl;
-}
-
-Ingredient::Ingredient(Ingredient&& other) noexcept
-    : Item(std::move(other.name)), quantity(other.quantity), unit(std::move(other.unit)) {
-    std::cout << "Moving Ingredient: " << name << std::endl;
-}
-
-Ingredient::~Ingredient() {
-    std::cout << "Ingredient removed: " << name << std::endl;
-}
-
-Ingredient& Ingredient::operator=(const Ingredient& other) {
-    if (this != &other) {
-        name = other.name;
-        quantity = other.quantity;
-        unit = other.unit;
-    }
-    return *this;
-}
+    : name(name), quantity(quantity), unit(unit) {}
 
 void Ingredient::display() const {
-    std::cout << name << " " << quantity << " " << unit << std::endl;
+    std::cout << name << ": " << quantity << " " << unit << std::endl;
 }
-
-std::ostream& operator<<(std::ostream& os, const Ingredient& ingredient) {
-    os << ingredient.name << " " << ingredient.quantity << " " << ingredient.unit;
-    return os;
-}
-
-std::istream& operator>>(std::istream& is, Ingredient& ingredient) {
-    is >> ingredient.name >> ingredient.quantity >> ingredient.unit;
-    return is;
-}
-
-
--- Ingredient.h
+++ Ingredient.h
@@ -1,29 +1,19 @@
 #ifndef INGREDIENT_H
 #define INGREDIENT_H
+
 #include <string>
+#include <iostream>
 
-#include "Item.h"
-
-class Ingredient : public Item {
+class Ingredient {
 private:
+    std::string name;
     double quantity;
     std::string unit;
 
 public:
     Ingredient(const std::string& name = "Unknown", double quantity = 0, const std::string& unit = "pcs");
-    Ingredient(const Ingredient& other);
-    Ingredient(Ingredient&& other) noexcept;
-    ~Ingredient();
 
-    Ingredient& operator=(const Ingredient& other);
-    Ingredient& updateQuantity(double newQuantity);
-
-    void display() const override;
-
-    friend std::ostream& operator<<(std::ostream& os, const Ingredient& ingredient);
-    friend std::istream& operator>>(std::istream& is, Ingredient& ingredient);
-    friend Ingredient operator+(const Ingredient& lhs, const Ingredient& rhs);
-    Ingredient operator-() const;
+    void display() const;
 };
 
 #endif // INGREDIENT_H
