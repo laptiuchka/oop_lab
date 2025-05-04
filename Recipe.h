c2f20d0a541942ac3da35aec535e434848b10ca4
laptiuchka <tanialapik@gmail.com>
2025-05-04 19:13:52 +0300

    Added class with ingredient support and impl

--- Recipe.h
+++ Recipe.h
@@ -1,23 +1,16 @@
 #ifndef RECIPE_H
 #define RECIPE_H
+
 #include "AbstractRecipe.h"
-#include <vector>
 
 class Recipe : public AbstractRecipe {
-protected:
+private:
     std::string instructions;
 
 public:
     Recipe(const std::string& title, const std::string& instructions);
-    Recipe();
-    Recipe(const Recipe& other);
-    Recipe(Recipe&& other) noexcept;
-    ~Recipe();
-
-    Recipe& operator=(const Recipe& other);
-
-    void addIngredient(const Ingredient& ingredient);
-    void display() const override;
+    void display() const override final;
+    std::string getType() const override;
 };
 
 #endif // RECIPE_H
--- Recipee.cpp
+++ Recipee.cpp
@@ -1,32 +1,16 @@
 #include "Recipe.h"
+#include <iostream>
 
 Recipe::Recipe(const std::string& title, const std::string& instructions)
-: AbstractRecipe(title), instructions(instructions) {}
-
-Recipe::Recipe() : AbstractRecipe("Unknown"), instructions("No instructions") {}
-
-Recipe::Recipe(const Recipe& other) : AbstractRecipe(other.title), instructions(other.instructions), ingredients(other.ingredients) {}
-
-Recipe::Recipe(Recipe&& other) noexcept : AbstractRecipe(std::move(other.title)), instructions(std::move(other.instructions)), ingredients(std::move(other.ingredients)) {}
-
-Recipe::~Recipe() {}
-
-Recipe& Recipe::operator=(const Recipe& other) {
-    if (this != &other) {
-        title = other.title;
-        instructions = other.instructions;
-        ingredients = other.ingredients;
-    }
-    return *this;
-}
-
-void Recipe::addIngredient(const Ingredient& ingredient) {
-    ingredients.push_back(ingredient);
-}
+    : AbstractRecipe(title), instructions(instructions) {}
 
 void Recipe::display() const {
     std::cout << "Recipe: " << title << "\nInstructions: " << instructions << "\nIngredients:\n";
     for (const auto& ing : ingredients) {
         ing.display();
     }
+}
+
+std::string Recipe::getType() const {
+    return "Basic Recipe";
 }
