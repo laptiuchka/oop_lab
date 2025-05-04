7e795e1d9ccb0622d33e6e32de32325cd3ed08e1
laptiuchka <tanialapik@gmail.com>
2025-05-04 19:15:11 +0300

    Add categories and difficulties

--- AdvancedRecipe.cpp
+++ AdvancedRecipe.cpp
@@ -1,9 +1,15 @@
 #include "AdvancedRecipe.h"
+#include <iostream>
 
-AdvancedRecipe::AdvancedRecipe(const std::string& title, const std::string& instructions, const std::string& category, int difficulty)
-: Recipe(title, instructions), category(category), difficultyLevel(difficulty) {}
+AdvancedRecipe::AdvancedRecipe(const std::string& title, const std::string& instructions,
+                               const std::string& category, int difficulty)
+    : Recipe(title, instructions), category(category), difficulty(difficulty) {}
 
 void AdvancedRecipe::display() const {
     Recipe::display();
-    std::cout << "Category: " << category << "\nDifficulty: " << difficultyLevel << "/5\n";
+    std::cout << "Category: " << category << ", Difficulty: " << difficulty << std::endl;
+}
+
+std::string AdvancedRecipe::getType() const {
+    return "Advanced Recipe";
 }
--- AdvancedRecipe.h
+++ AdvancedRecipe.h
@@ -1,15 +1,18 @@
 #ifndef ADVANCEDRECIPE_H
 #define ADVANCEDRECIPE_H
+
 #include "Recipe.h"
 
 class AdvancedRecipe : public Recipe {
 private:
     std::string category;
-    int difficultyLevel;
+    int difficulty;
 
 public:
-    AdvancedRecipe(const std::string& title, const std::string& instructions, const std::string& category, int difficulty);
+    AdvancedRecipe(const std::string& title, const std::string& instructions,
+                   const std::string& category, int difficulty);
     void display() const override;
+    std::string getType() const override;
 };
 
 #endif // ADVANCEDRECIPE_H
