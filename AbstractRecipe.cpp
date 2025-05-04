760202a09ddea86153976506f03dfbefff59e63a
laptiuchka <tanialapik@gmail.com>
2025-05-04 19:12:27 +0300

    Added abstract class with virtual display

--- AbstractRecipe.cpp
+++ AbstractRecipe.cpp
@@ -1,3 +1,14 @@
 #include "AbstractRecipe.h"
+#include <iostream>
 
 AbstractRecipe::AbstractRecipe(const std::string& title) : title(title) {}
+
+AbstractRecipe::~AbstractRecipe() {}
+
+void AbstractRecipe::addIngredient(const Ingredient& ingredient) {
+    ingredients.push_back(ingredient);
+}
+
+void AbstractRecipe::printType() const {
+    std::cout << "This is a recipe of type: " << getType() << std::endl;
+}
--- AbstractRecipe.h
+++ AbstractRecipe.h
@@ -1,17 +1,24 @@
 #ifndef ABSTRACTRECIPE_H
 #define ABSTRACTRECIPE_H
-#include <vector>
-#include "Ingredient.h"
 
-class AbstractRecipe {
+#include <vector>
+#include <string>
+#include "Ingredient.h"
+#include "Printable.h"
+
+class AbstractRecipe : public Printable {
 protected:
     std::string title;
     std::vector<Ingredient> ingredients;
 
 public:
     AbstractRecipe(const std::string& title);
-    virtual ~AbstractRecipe() {}
+    virtual ~AbstractRecipe();
+
+    virtual void addIngredient(const Ingredient& ingredient);
     virtual void display() const = 0;
+    virtual std::string getType() const = 0;
+    void printType() const override;
 };
 
 #endif // ABSTRACTRECIPE_H
